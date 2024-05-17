545828f4244226d886d7b04015e777804c7d5699
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-17 19:47:20 +0900

    feat #6 - VocaBookViewController 설정

--- EveryDayVoca/Controllers/VocaBookViewController.swift
+++ EveryDayVoca/Controllers/VocaBookViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class VocaBookViewController: BaseViewController {
+final class VocaBookViewController: BaseViewController {
+    
+    // MARK: - Properties
+    let vocaBookVocaListView = VocaBookVocaListView()
+    let titleLabel = UILabel().then {
+        $0.text = "나의 단어 사전"
+        $0.font = UIFont.pretendard(size: 17, weight: .semibold)
+        $0.textColor = UIColor.evText
+        $0.textAlignment = .center
+    }
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        view = vocaBookVocaListView
+        self.navigationItem.titleView = titleLabel
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    
     
 }
