6f427bbc3ea23b39e3e90282a7fc766da106fbe7
KJH <gh7052@gmail.com>
2024-05-20 02:35:15 +0900

    add: #8 - StudyAlertView, StudyAlertViewController

---
+++ EveryDayVoca/Views/Study/StudyAlertView.swift
@@ -0,0 +1,20 @@
+//
+//  StudyAlertView.swift
+//  EveryDayVoca
+//
+//  Created by 김정호 on 5/20/24.
+//
+
+import UIKit
+
+class StudyAlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
