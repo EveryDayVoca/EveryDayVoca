c0793093aa85906042c6f5f0698960647f627a69
KJH <gh7052@gmail.com>
2024-05-19 20:34:06 +0900

    feat: #8 - BaseCollectionViewCell 베이스 구성

--- EveryDayVoca/Views/Bases/BaseCollectionViewCell.swift
+++ EveryDayVoca/Views/Bases/BaseCollectionViewCell.swift
@@ -6,7 +6,32 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class BaseCollectionViewCell: UICollectionViewCell {
+class BaseCollectionViewCell: UICollectionViewCell, BaseViewProtocol {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configureUI() {
+        self.backgroundColor = .evBackground
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureConstraints() { }
 }
