6a24e502bf7dbb05913cb53df53e17145caa4a4b
KJH <gh7052@gmail.com>
2024-05-21 01:29:03 +0900

    feat: #8 - StudyView UI 구성

--- EveryDayVoca/Views/Study/StudyView.swift
+++ EveryDayVoca/Views/Study/StudyView.swift
@@ -7,14 +7,51 @@
 
 import UIKit
 
-class StudyView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class StudyView: BaseView {
+    
+    // MARK: - properties
+    let titleLabel = UILabel().then {
+        $0.text = "단어장"
+        $0.textColor = .evText
+        $0.textAlignment = .center
+        $0.font = UIFont.pretendard(size: 17, weight: .semibold)
     }
-    */
-
+    
+    let backBarButtonItem = UIBarButtonItem().then {
+        $0.image = .chevronLeft.withRenderingMode(.alwaysTemplate)
+        $0.tintColor = .evText
+        $0.style = .done
+    }
+    
+    let plusBarButtonItem = UIBarButtonItem().then {
+        $0.image = .plus.withRenderingMode(.alwaysTemplate)
+        $0.tintColor = .evText
+        $0.style = .done
+    }
+    
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.contentInset = UIEdgeInsets(top: 32, left: 32, bottom: 32, right: 32)
+    }
+    
+    // MARK: - life cycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - methods
+    override func configureHierarchy() {
+        self.addSubview(collectionView)
+    }
+    
+    override func configureConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
 }
