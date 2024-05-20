15236d0d123831111f9ddf6a93a46fed07bf5d72
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-20 10:41:06 +0900

    feat #10 - VC 파일 생성

---
+++ EveryDayVoca/Controllers/MyVocaFlashCard/MyVocaFlashCardViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MyVocaFlashCardViewController.swift
+//  EveryDayVoca
+//
+//  Created by 유림 on 5/20/24.
+//
+
+import UIKit
+
+class MyVocaFlashCardViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Life Cycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: - Methods
+    
+    override func configureStyle() { 
+        super.configureStyle()
+    }
+    
+    override func configureDelegate() {
+        super.configureDelegate()
+    }
+    
+    override func bind() {
+        super.bind()
+    }
+
+}
