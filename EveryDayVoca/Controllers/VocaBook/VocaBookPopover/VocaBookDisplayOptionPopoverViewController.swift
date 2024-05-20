3a32d72582a0e7d10ec2c5f1ba2e477d59ff70a2
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-20 18:56:21 +0900

    feat #14 - 디스플레이 옵션 팝오버 VC 설정

--- EveryDayVoca/Controllers/VocaBook/VocaBookPopover/VocaBookDisplayOptionPopoverViewController.swift
+++ EveryDayVoca/Controllers/VocaBook/VocaBookPopover/VocaBookDisplayOptionPopoverViewController.swift
@@ -7,23 +7,78 @@
 
 import UIKit
 
-class VocaBookDisplayOptionPopoverViewController: UIViewController {
+enum DisplayOption {
+    case englishOnly
+    case koreanOnly
+    case englishAndKorean
+}
+
+class VocaBookDisplayOptionPopoverViewController: BaseViewController {
+
+    // MARK: - Properties
+    
+    private let vocaBookPopoverView = VocaBookPopoverView()
+    var displayOptions:[DisplayOption] = [.englishOnly, .koreanOnly, .englishAndKorean]
+    var selectedOption: DisplayOption = .englishAndKorean
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        view = vocaBookPopoverView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-    */
+    
+    // MARK: - Methods
+    
+    override func configureStyle() {
+        super.configureStyle()
+    }
+    
+    override func configureDelegate() {
+        super.configureDelegate()
+        
+//        let tableView = vocaBookPopoverView.tableView
+        
+        self.vocaBookPopoverView.tableView.register(VocaBookDisplayOptionPopoverTableViewCell.self, forCellReuseIdentifier: VocaBookDisplayOptionPopoverTableViewCell.identifier)
+        self.vocaBookPopoverView.tableView.dataSource = self
+        self.vocaBookPopoverView.tableView.delegate = self
+        
+        self.vocaBookPopoverView.tableView.allowsMultipleSelection = true
+    }
+}
 
+extension VocaBookDisplayOptionPopoverViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: VocaBookDisplayOptionPopoverTableViewCell.identifier) as? VocaBookDisplayOptionPopoverTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        cell.bind(displayOption: displayOptions[indexPath.row], selectedOption: selectedOption)
+        //        cell.selectionStyle = .none
+        return cell
+    }
+    
+    
+}
+
+extension VocaBookDisplayOptionPopoverViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedOption = displayOptions[indexPath.row]
+        tableView.reloadData()
+    }
+    
 }
