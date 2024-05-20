efda413357a93b3333071ed94d84b00cb09dc876
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-20 10:17:22 +0900

    fix #6 - 변수명 수정, vocaBookLabel 텍스트크기 수정

--- EveryDayVoca/Views/VocaBook/VocaBookBaseView.swift
+++ EveryDayVoca/Views/VocaBook/VocaBookBaseView.swift
@@ -11,18 +11,18 @@ class VocaBookBaseView: BaseView {
 
     // MARK: - Properties
     
-    private let vocaBookSelectionView = UIView().then {
+    let vocaBookSelectActionView = UIView().then {
         $0.backgroundColor = .none
     }
     
-    private let vocaBookSelectionLabel = UILabel().then {
+    let currentVocaBookLabel = UILabel().then {
         $0.textAlignment = .center
         $0.textColor = UIColor.evText
         $0.numberOfLines = 1
-        $0.font = UIFont.pretendard(size: 12, weight: .medium)
+        $0.font = UIFont.pretendard(size: 14, weight: .medium)
     }
     
-    private let vocaBookSelectionImage = UIImageView().then {
+    private let vocaBookChevronImage = UIImageView().then {
         $0.image = UIImage(systemName: "chevron.forward")
         $0.contentMode = .scaleAspectFit
         $0.tintColor = UIColor.evText
@@ -34,31 +34,32 @@ class VocaBookBaseView: BaseView {
     // MARK: - Methods
     
     override func configureHierarchy() {
-        [vocaBookSelectionView,
+        [vocaBookSelectActionView,
          customView].forEach { self.addSubview($0) }
         
-        [vocaBookSelectionLabel,
-         vocaBookSelectionImage].forEach { vocaBookSelectionView.addSubview($0) }
+        [currentVocaBookLabel,
+         vocaBookChevronImage].forEach { vocaBookSelectActionView.addSubview($0) }
         
     }
     
     override func configureConstraints() {
-        vocaBookSelectionView.snp.makeConstraints {
+        vocaBookSelectActionView.snp.makeConstraints {
             $0.top.horizontalEdges.equalTo(self.safeAreaLayoutGuide)
             $0.height.equalTo(52)
         }
         
-        vocaBookSelectionLabel.snp.makeConstraints {
+        currentVocaBookLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
         
-        vocaBookSelectionImage.snp.makeConstraints {
+        vocaBookChevronImage.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(20)
             $0.centerY.equalToSuperview()
+            $0.size.equalTo(14)
         }
         
         customView.snp.makeConstraints {
-            $0.top.equalTo(vocaBookSelectionView.snp.bottom)
+            $0.top.equalTo(vocaBookSelectActionView.snp.bottom)
             $0.horizontalEdges.bottom.equalTo(self.safeAreaLayoutGuide)
         }
     }
