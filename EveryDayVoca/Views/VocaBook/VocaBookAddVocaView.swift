c4e462cddfd98fafec1996fe42d85753ac9bf8c3
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-21 12:47:45 +0900

    feat #15 - View components 계층, 제약조건 설정

--- EveryDayVoca/Views/VocaBook/VocaBookAddVocaView.swift
+++ EveryDayVoca/Views/VocaBook/VocaBookAddVocaView.swift
@@ -11,6 +11,8 @@ class VocaBookAddVocaView: BaseView {
     
     // MARK: - Properties
     
+    var vocaBooks: [String] = ["나의 단어장", "고등 영단어"]
+    
     let titleLabel = UILabel().then {
         $0.textAlignment = .center
         $0.textColor = UIColor.evText
@@ -40,6 +42,44 @@ class VocaBookAddVocaView: BaseView {
         $0.spellCheckingType = .yes
     }
     
+    lazy var vocaBookSelectPopupButton = UIButton().then {
+        $0.backgroundColor = UIColor.evBackground
+        $0.setTitle("단어장 선택하기", for: .normal)
+        $0.setTitleColor(UIColor.evText, for: .normal)
+        $0.showsMenuAsPrimaryAction = true
+        $0.changesSelectionAsPrimaryAction = true
+        $0.menu = vocaBookButtonMenu
+    }
+    
+    lazy var vocaBookButtonMenu = { [self] in
+        var actionArray: [UIAction] = []
+        vocaBooks.forEach {
+            actionArray.append(makeUIAction(title: $0))
+        }
+        let buttonMenu = UIMenu(title: "단어장 선택", children: actionArray)
+        return buttonMenu
+    }()
+    
+    let buttonStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 24
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
+    }
+    
+    lazy var cancelButton = UIButton(
+        configuration:
+            returnButtonConfiguration(
+                title: "취소하기", fgColor: UIColor.blue100, bgColor: UIColor.evBackground, isBordered: true)
+    )
+    
+    lazy var addButton = UIButton(
+        configuration:
+            returnButtonConfiguration(
+                title: "추가하기", fgColor: UIColor.evBackground, bgColor: UIColor.blue100, isBordered: false)
+    )
+    
+    
     
     // MARK: - Methods
 
@@ -51,12 +91,70 @@ class VocaBookAddVocaView: BaseView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func configureUI() {
-        self.backgroundColor = .evBackground
+    override func configureHierarchy() { 
+        super.configureHierarchy()
+        
+        [titleLabel,
+        englishTextField,
+        koreanTextField,
+        vocaBookSelectPopupButton,
+         buttonStackView].forEach { self.addSubview($0) }
+        
+        [cancelButton,
+         addButton].forEach { buttonStackView.addArrangedSubview($0) }
     }
     
-    override func configureHierarchy() { }
-    
-    override func configureConstraints() { }
+    override func configureConstraints() { 
+        super.configureConstraints()
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self).offset(30)
+            $0.centerX.equalTo(self)
+        }
+        
+        englishTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(self).inset(20)
+            $0.height.equalTo(44)
+        }
+        
+        koreanTextField.snp.makeConstraints {
+            $0.top.equalTo(englishTextField.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(englishTextField)
+            $0.height.equalTo(44)
+        }
+        
+        vocaBookSelectPopupButton.snp.makeConstraints {
+            $0.top.equalTo(koreanTextField.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(koreanTextField)
+            $0.height.equalTo(44)
+        }
+    }
 
+    
+    
+    private func makeUIAction(title: String) -> UIAction {
+        return UIAction(title: title, handler: { _ in print(title) })
+    }
+    
+    private func returnButtonConfiguration(title: String, fgColor: UIColor, bgColor: UIColor, isBordered: Bool) -> UIButton.Configuration {
+        
+        
+        var config = UIButton.Configuration.plain()
+        // title
+        config.attributedTitle = AttributedString(title)
+        config.attributedTitle?.font = UIFont.pretendard(size: 20, weight: .semibold)
+        // color
+        config.baseForegroundColor = fgColor
+        config.background.backgroundColor = bgColor
+        // frame
+        config.background.cornerRadius = 15
+        if isBordered {
+            config.background.strokeWidth = 1
+            config.background.strokeColor = UIColor.blue100
+        }
+        return config
+    }
+    
+    
 }
