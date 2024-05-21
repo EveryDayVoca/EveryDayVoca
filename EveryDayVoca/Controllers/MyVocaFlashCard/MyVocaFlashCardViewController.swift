ae5e7e237df9768b580efaa26c979e35417c9a24
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-21 15:56:50 +0900

    feat #15 - navigationController 설정

--- EveryDayVoca/Controllers/MyVocaFlashCard/MyVocaFlashCardViewController.swift
+++ EveryDayVoca/Controllers/MyVocaFlashCard/MyVocaFlashCardViewController.swift
@@ -20,6 +20,7 @@ class MyVocaFlashCardViewController: BaseViewController {
     override func loadView() {
         view = myVocaFlashCardView
         self.tabBarController?.tabBar.isHidden = true
+        setNavigationController()
     }
     
     override func viewDidLoad() {
@@ -45,5 +46,29 @@ class MyVocaFlashCardViewController: BaseViewController {
     func changeIndex(index: Int) {
         self.index = index
     }
-
+    
+    func setNavigationController() {
+        let navigationBarTitleLabel = UILabel().then {
+            $0.text = "나의 단어 사전"
+            $0.font = UIFont.pretendard(size: 17, weight: .semibold)
+            $0.textAlignment = .center
+        }
+        
+        let navigationBarPlusButton = UIBarButtonItem().then {
+            $0.image = UIImage(named: "plus")
+            $0.target = self
+            $0.action = #selector(tappedNavigationBarPlusButton)
+        }
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.evText
+        self.navigationItem.titleView = navigationBarTitleLabel
+        self.navigationItem.rightBarButtonItem = navigationBarPlusButton
+        self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
+    }
+    
+    @objc func tappedNavigationBarPlusButton() {
+        let addVocaVC = VocaBookAddVocaViewController()
+        present(addVocaVC, animated: true)
+    }
+    
 }
