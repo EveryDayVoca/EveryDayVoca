b34e8c61ffe5cd977b5f8f8bfdc4de07d87e7a66
KJH <gh7052@gmail.com>
2024-05-21 14:11:05 +0900

    add: #16 - StudyView

---
+++ EveryDayVoca/Views/StudyView.swift
@@ -0,0 +1,20 @@
+//
+//  StudyView.swift
+//  EveryDayVoca
+//
+//  Created by 김정호 on 5/21/24.
+//
+
+import UIKit
+
+class StudyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
