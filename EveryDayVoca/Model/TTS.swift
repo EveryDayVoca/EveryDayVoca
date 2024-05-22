b3eff5e76aef9a49a445463d4745aa3537f7b61e
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-05-22 12:45:04 +0900

    add: #21 - TTS 구현

---
+++ EveryDayVoca/Model/TTS.swift
@@ -0,0 +1,33 @@
+//
+//  TTS.swift
+//  EveryDayVoca
+//
+//  Created by 배지해 on 5/22/24.
+//
+
+import AVFoundation
+
+enum Language: String {
+    case korean = "ko-KR"
+    case english = "en-US"
+}
+
+class TTS {
+    
+    private let synthesizer = AVSpeechSynthesizer()
+    
+    func play(str: String, language: Language) {
+        print("TTS 시작")
+        let utterance = AVSpeechUtterance(string: str)
+        utterance.voice = AVSpeechSynthesisVoice(language: language.rawValue)
+        utterance.rate = 0.5
+        utterance.pitchMultiplier = 0.8
+        synthesizer.stopSpeaking(at: .immediate)
+        synthesizer.speak(utterance)
+    }
+    
+    func stop() {
+        print("TTS 종료")
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+}
