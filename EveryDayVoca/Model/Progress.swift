51b1e9476f4d15e412f7b2ed43142fde123438bc
KJH <gh7052@gmail.com>
2024-05-23 21:06:39 +0900

    add: #37 - User, Progress

---
+++ EveryDayVoca/Model/Progress.swift
@@ -0,0 +1,16 @@
+//
+//  Progress.swift
+//  EveryDayVoca
+//
+//  Created by 김정호 on 5/23/24.
+//
+
+import Foundation
+
+struct Progress: Codable {
+    var lv1: Int
+    var lv2: Int
+    var lv3: Int
+    var lv4: Int
+    var lv5: Int
+}
