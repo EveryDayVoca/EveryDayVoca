67f835ad4ff497d5c270ea1b4d5f512896e2dee6
yurim830 <157277372+yurim830@users.noreply.github.com>
2024-05-23 18:30:28 +0900

    feat #35 - 기본 UIComponents 배치

--- EveryDayVoca/Controllers/Report/SecondViewController.swift
+++ EveryDayVoca/Controllers/Report/SecondViewController.swift
@@ -19,28 +19,44 @@ final class SecondViewController: BaseViewController {
     
     override func loadView() {
         view = secondView
+        setNavigationController()
+        print("loadView")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .evBackground
+        print("viewDidLoad")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.tabBarController?.tabBar.isHidden = true
+        print("viewWillAppear")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.tabBarController?.tabBar.isHidden = false
+        print("viewWillDisappear")
     }
     
     
     
     // MARK: - Methods
+    
+    func setNavigationController() {
+        let navigationBarTitleLabel = UILabel().then {
+            $0.text = "일일 학습량"
+            $0.font = UIFont.pretendard(size: 17, weight: .semibold)
+            $0.textAlignment = .center
+        }
+        self.navigationItem.titleView = navigationBarTitleLabel
+    }
+    
+    
     func bind(dateComponents: DateComponents?) {
+        print("bind")
+        
         guard let date = dateComponents?.date else { return }
         let vocaDate = VocaCoreDataManager.shared.getVocaDateData(date: date)
         
@@ -51,12 +67,19 @@ final class SecondViewController: BaseViewController {
         self.dailyStudyData = VocaCoreDataManager.shared
             .getStudyData(index: leadWordIndex, count: studiedWordCount)
         
+//        self.dailyStudyData = [.difficult:5, .ambiguous:7, .memorized:10, .none:5]
         let difficult = dailyStudyData[.difficult] ?? 0
         let ambiguous = dailyStudyData[.ambiguous] ?? 0
         let memorized = dailyStudyData[.memorized] ?? 0
         
-        secondView.reConfigureStatusBar(difficult: difficult, ambiguous: ambiguous, memorized: memorized)
+        
+        // levelLabel
+        secondView.levelLabel.text = "1단계"
+        
+        // statusBar
+        secondView.remakeConstraints(difficult: difficult, ambiguous: ambiguous, memorized: memorized)
     }
+    
     
     
 }
--- EveryDayVoca/Views/Report/SecondView.swift
+++ EveryDayVoca/Views/Report/SecondView.swift
@@ -11,7 +11,7 @@ class SecondView: BaseView {
     
     // MARK: - Properties
     
-    let levelTitle = UILabel().then {
+    let levelLabel = UILabel().then {
         $0.textAlignment = .center
         $0.textColor = UIColor.evText
         $0.numberOfLines = 1
@@ -35,6 +35,7 @@ class SecondView: BaseView {
     }
     
     let memorizedBar = UIView().then {
+        print("객체 생성")
         $0.backgroundColor = UIColor.blue10
         $0.layer.cornerRadius = 10
         $0.layer.shadowOpacity = 0.2
@@ -42,15 +43,42 @@ class SecondView: BaseView {
         $0.layer.shadowPath = nil
     }
     
+    let dateLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.textColor = UIColor.evText
+        $0.numberOfLines = 1
+        $0.font = UIFont.pretendard(size: 17, weight: .semibold)
+    }
+    
+    let yesterDayButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        $0.tintColor = UIColor.evText
+    }
+    
+    let tomorrowButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        $0.tintColor = UIColor.evText
+        
+    }
+    
+    let vocaListTableView = UITableView().then {
+        $0.backgroundColor = UIColor.evBackground
+        $0.separatorStyle = .none
+    }
+    
     
     
     // MARK: - Methods
     
     override func configureHierarchy() {
-        [levelTitle,
+        [levelLabel,
          difficultBar,
          ambiguousBar,
-         memorizedBar].forEach{ self.addSubview($0) }
+         memorizedBar,
+         dateLabel,
+         yesterDayButton,
+         tomorrowButton,
+         vocaListTableView].forEach{ self.addSubview($0) }
     }
     
     
@@ -58,33 +86,60 @@ class SecondView: BaseView {
         
         let statusBarWidth = UIScreen.main.bounds.width - 52
         
-        levelTitle.snp.makeConstraints {
+        levelLabel.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide).offset(16)
             $0.centerX.equalTo(safeAreaLayoutGuide)
         }
         
         difficultBar.snp.makeConstraints {
-            $0.top.equalTo(levelTitle.snp.bottom).offset(16)
+            $0.top.equalTo(levelLabel.snp.bottom).offset(16)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(26)
+            $0.height.equalTo(25)
             $0.width.equalTo(statusBarWidth)
         }
         
         ambiguousBar.snp.makeConstraints {
-            $0.top.equalTo(levelTitle.snp.bottom).offset(16)
+            $0.top.equalTo(levelLabel.snp.bottom).offset(16)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(26)
+            $0.height.equalTo(25)
             $0.width.equalTo(statusBarWidth)
         }
         
         memorizedBar.snp.makeConstraints {
-            $0.top.equalTo(levelTitle.snp.bottom).offset(16)
+            $0.top.equalTo(levelLabel.snp.bottom).offset(16)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(26)
+            $0.height.equalTo(25)
             $0.width.equalTo(statusBarWidth)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(difficultBar.snp.bottom).offset(22)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(42)
+        }
+        
+        yesterDayButton.snp.makeConstraints {
+            $0.centerY.equalTo(dateLabel)
+            $0.leading.equalTo(difficultBar)
+            $0.size.equalTo(17)
+        }
+        
+        tomorrowButton.snp.makeConstraints {
+            $0.centerY.equalTo(dateLabel)
+            $0.trailing.equalTo(difficultBar)
+            $0.size.equalTo(17)
+        }
+        
+        vocaListTableView.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(12)
+            $0.horizontalEdges.equalTo(difficultBar)
+            $0.bottom.equalTo(safeAreaLayoutGuide)
         }
     }
     
     
-    func reConfigureStatusBar(difficult: Int, ambiguous: Int, memorized: Int) {
-        
+    func remakeConstraints(difficult: Int, ambiguous: Int, memorized: Int) {
+        print("reConfigure")
         let statusBarWidth = UIScreen.main.bounds.width - 52
         
         let total = CGFloat(difficult + ambiguous + memorized)
@@ -99,20 +154,23 @@ class SecondView: BaseView {
         
         
         difficultBar.snp.remakeConstraints {
-            $0.top.equalTo(levelTitle.snp.bottom).offset(16)
+            $0.top.equalTo(levelLabel.snp.bottom).offset(16)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(26)
+            $0.height.equalTo(25)
             $0.width.equalTo(difficultBarWidth)
         }
         
         ambiguousBar.snp.remakeConstraints {
-            $0.top.equalTo(levelTitle.snp.bottom).offset(16)
+            $0.top.equalTo(levelLabel.snp.bottom).offset(16)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(26)
+            $0.height.equalTo(25)
             $0.width.equalTo(ambiguousBarWidth)
         }
         
         memorizedBar.snp.remakeConstraints {
-            $0.top.equalTo(levelTitle.snp.bottom).offset(16)
+            $0.top.equalTo(levelLabel.snp.bottom).offset(16)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(26)
+            $0.height.equalTo(25)
             $0.width.equalTo(memorizedBarWidth)
         }
         
